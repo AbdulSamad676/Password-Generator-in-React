2e4558367fcbe806bffa62d1e27b5f89f421d223
Abdul Samad Khan <abdulsamad31650@gmail.com>
2024-01-28 15:44:38 +0500

    password generated and copy text functionality remains

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -7,6 +7,24 @@ function App() {
 	const [length, setLength] = useState(8);
 	const [password, setPassword] = useState('');
 	const [numberAllowed, setNumberAllowed] = useState(false);
+	const [charAllowed, setcharAllowed] = useState(false);
+
+	let passwordGenerator = useCallback(() => {
+		let pass = '';
+		let str = 'ABDCEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+		if (numberAllowed) str += '1234567890';
+		if (charAllowed) str += '!@#$%^&*()_+{}[]';
+
+		for (let i = 0; i < length; i++) {
+			let char = Math.floor(Math.random() * str.length + 1);
+			pass += str.charAt(char);
+		}
+		setPassword(pass);
+	}, [length, numberAllowed, charAllowed, setPassword]);
+
+	useEffect(() => {
+		passwordGenerator();
+	}, [length, numberAllowed, charAllowed, setPassword]);
 
 	return (
 		<>
@@ -14,9 +32,9 @@ function App() {
 				Password Generator in React
 			</h1>
 			<div className='mx-auto w-full max-w-md  p-5 mt-10 rounded-xl bg-gray-800 shadow-lg'>
-				<div className='flex items-center border p-3 rounded mb-5'>
+				<div className='flex  items-center  p-3 rounded mb-5'>
 					<input
-						className='text-black px-2 py-1 text-xl rounded w-60'
+						className='text-black w-full px-2 py-1 text-xl rounded w-60'
 						type='text'
 						placeholder='Pasword Here'
 						value={password}
@@ -26,7 +44,7 @@ function App() {
 						Copy
 					</button>
 				</div>
-				<div className='flex  gap-x-2 border'>
+				<div className='flex  gap-x-5 '>
 					<div className='flex items-center gap-x-1 '>
 						<input
 							type='range'
@@ -44,13 +62,19 @@ function App() {
 						<input
 							type='checkbox'
 							defaultChecked={numberAllowed}
-							min={6}
-							max={100}
-							value={length}
 							className='cursor-pointer py-auto'
 							onChange={() => setNumberAllowed(prev => !prev)}
 						/>
 						<label>Numbers</label>
+					</div>
+					<div className='flex items-center gap-x-1'>
+						<input
+							type='checkbox'
+							defaultChecked={charAllowed}
+							className='cursor-pointer py-auto'
+							onChange={() => setcharAllowed(prev => !prev)}
+						/>
+						<label>Charecter</label>
 					</div>
 				</div>
 			</div>
